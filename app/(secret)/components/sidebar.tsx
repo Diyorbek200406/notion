a62eb86721620a21d9d07d46494b5f7848ae2133
Erkinov Diyorbek <erkinovdiyorbek39@gmail.com>
2023-12-18 17:08:11 +0500

    sidebar responsive

--- app/(secret)/components/sidebar.tsx
+++ app/(secret)/components/sidebar.tsx
@@ -2,15 +2,24 @@
 
 import { cn } from "@/lib/utils";
 import { ChevronsLeft, MenuIcon } from "lucide-react";
-import { ElementRef, useRef, useState } from "react";
+import { ElementRef, useEffect, useRef, useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
 
 const Sidebar = () => {
+  const isMobile = useMediaQuery("(max-width: 770px)");
   const sidebarRef = useRef<ElementRef<"div">>(null);
   const navbarRef = useRef<ElementRef<"div">>(null);
   const isResizing = useRef(false);
-
-  const [isCollapse, setIsCollapse] = useState(false);
+  const [isCollapse, setIsCollapse] = useState(isMobile);
   const [isResetting, setIsResetting] = useState(false);
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse();
+    } else {
+      reset();
+    }
+  }, [isMobile]);
 
   const collapse = () => {
     if (sidebarRef.current && navbarRef.current) {
@@ -27,9 +36,9 @@ const Sidebar = () => {
     if (sidebarRef.current && navbarRef.current) {
       setIsCollapse(false);
       setIsResetting(true);
-      sidebarRef.current.style.width = "240px";
-      navbarRef.current.style.width = "calc(100% - 240px)";
-      navbarRef.current.style.left = "240px";
+      sidebarRef.current.style.width = isMobile ? "100%" : "240px";
+      navbarRef.current.style.width = isMobile ? "0" : "calc(100% - 240px)";
+      navbarRef.current.style.left = isMobile ? "100%" : "240px";
       setTimeout(() => setIsResetting(false), 300);
     }
   };
@@ -63,15 +72,15 @@ const Sidebar = () => {
 
   return (
     <>
-      <div className={cn("group/sidebar h-screen bg-secondary overflow-y-auto relative flex w-60 flex-col z-50", isResetting && "transition-all ease-in duration-300")} ref={sidebarRef}>
-        <div className="w-6 h-6 text-muted-foreground rounded-sm hover:bg-neutral-300 absolute right-2 top-3 dark:hover:bg-neutral-600 opacity-0 group-hover/sidebar:opacity-100 transition" role="button" onClick={collapse}>
+      <div className={cn("group/sidebar h-screen bg-secondary overflow-y-auto relative flex w-60 flex-col z-50", isResetting && "transition-all ease-in duration-300", isMobile && "w-0")} ref={sidebarRef}>
+        <div className={cn("w-6 h-6 text-muted-foreground rounded-sm hover:bg-neutral-300 absolute right-2 top-3 dark:hover:bg-neutral-600 opacity-0 group-hover/sidebar:opacity-100 transition", isMobile && "opacity-100")} role="button" onClick={collapse}>
           <ChevronsLeft className="w-6 h-6" />
         </div>
 
         <div className="absolute h-full w-1 right-0 top-0 cursor-ew-resize bg-primary/10 opacity-0 group-hover/sidebar:opacity-100 transition" onMouseDown={handleMouseDown} />
       </div>
 
-      <div className={cn("absolute top-0 z-50 left-60 w-[calc(100% - 240px)]", isResetting && "transition-all ease-in duration-300")} ref={navbarRef}>
+      <div className={cn("absolute top-0 z-50 left-60 w-[calc(100% - 240px)]", isResetting && "transition-all ease-in duration-300", isMobile && "w-full left-0")} ref={navbarRef}>
         <nav className="bg-transparent p-2 w-full">{isCollapse && <MenuIcon className="w-6 h-6 text-muted-foreground" role="button" onClick={reset} />}</nav>
       </div>
     </>
